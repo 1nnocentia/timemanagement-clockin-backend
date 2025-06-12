ee52ee54b6be42f5d3913bb0f9062e056108e2b9
Innocentia <innocentiahan@gmail.com>
2025-06-13 00:12:43 +0800

    add notif repo

---
+++ src/main/java/com/clockin/clockin/repository/NotificationRepository.java
@@ -0,0 +1,14 @@
+package com.clockin.clockin.repository;
+
+import com.clockin.clockin.model.Notification;
+import com.clockin.clockin.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByUserOrderByCreatedAtDesc(User user);
+    List<Notification> findByUserAndIsReadFalseOrderByCreatedAtDesc(User user);
+}
