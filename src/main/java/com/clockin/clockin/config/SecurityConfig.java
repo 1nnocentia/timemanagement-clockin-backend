ef0aeb6ef352adbd69c9fe99c2329af0927f7a89
Innocentia <innocentiahan@gmail.com>
2025-06-13 00:39:20 +0800

    hide mailing

--- src/main/java/com/clockin/clockin/config/SecurityConfig.java
+++ src/main/java/com/clockin/clockin/config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package com.clockin.clockin.config;
 
+import java.util.Properties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     // Anotasi @Bean menandakan bahwa metode ini akan menghasilkan bean yang dikelola oleh Spring IoC container
@@ -14,4 +21,40 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+     // ** Mendefinisikan JavaMailSender bean secara eksplisit **
+    // Menggunakan @Value untuk menyuntikkan nilai dari application.properties
+    @Value("${spring.mail.host}")
+    private String host;
+    @Value("${spring.mail.port}")
+    private int port;
+    @Value("${spring.mail.username}")
+    private String username;
+    @Value("${spring.mail.password}")
+    private String password;
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private boolean auth;
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private boolean starttlsEnable;
+
+    // Untuk sementara menangguhkan layanan email, Anda bisa mengkomentari metode @Bean di bawah ini.
+    // Jika dikomentari, MailService tidak akan dapat menemukan bean JavaMailSender.
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", auth);
+        props.put("mail.smtp.starttls.enable", starttlsEnable);
+        props.put("mail.debug", "true"); // Atur ke true untuk debugging lebih lanjut
+
+        return mailSender;
+    }
+
+    
 }
