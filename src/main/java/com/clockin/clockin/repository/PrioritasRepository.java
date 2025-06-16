8d09d550da73f2d91f9b17f4fc99f13f724c6af4
Innocentia <innocentiahan@gmail.com>
2025-06-16 13:30:36 +0800

    modify prirotasRepository.java

--- src/main/java/com/clockin/clockin/repository/PrioritasRepository.java
+++ src/main/java/com/clockin/clockin/repository/PrioritasRepository.java
@@ -1,10 +1,20 @@
 package com.clockin.clockin.repository;
 
+import com.clockin.clockin.model.Kategori;
 import com.clockin.clockin.model.Prioritas;
+import com.clockin.clockin.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PrioritasRepository extends JpaRepository<Prioritas, Long> {
+    Optional<Kategori> findByNamaPrioritasAndUser(String namaPrioritas, User user);
+    List<Kategori> findByUser(User user);
+    Optional<Kategori> findByIdAndUser(Long id, User user);
 }
+
 
